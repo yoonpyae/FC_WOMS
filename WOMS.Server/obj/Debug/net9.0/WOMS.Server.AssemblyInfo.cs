c0456fae4ff6cfc36d71b332da6d3fd0bf7d64a6
ypeain <yoonpyae26@gmail.com>
2025-08-23 19:37:50 +0630

    Update Project

--- WOMS.Server/obj/Debug/net9.0/WOMS.Server.AssemblyInfo.cs
+++ WOMS.Server/obj/Debug/net9.0/WOMS.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WOMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231c901855b5af8e01a4bc065701e3bb266f8c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd72c8a9f8dad6d1ad6cfc7dddb21cd9cf2cb77")]
 [assembly: System.Reflection.AssemblyProductAttribute("WOMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WOMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
