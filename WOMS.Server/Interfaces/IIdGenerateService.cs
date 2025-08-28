0dad3efccdc6e4423f350585b1b52b2216610552
ypeain <yoonpyae26@gmail.com>
2025-08-28 15:09:52 +0630

    Update IIdGenerateService.cs

--- WOMS.Server/Interfaces/IIdGenerateService.cs
+++ WOMS.Server/Interfaces/IIdGenerateService.cs
@@ -6,77 +6,77 @@ public interface IIdGenerateService
     /// Item Code => [A0001]
     /// </summary>
     /// <param name="Name"></param>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    Task<string> GetItemCode(string Name, long HospitalId);
+    Task<string> GetItemCode(string Name, long ClinicId);
     
     /// <summary>
     /// Purchase VNO => [P1-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetPurchaseVno(long HospitalId);
+    string GetPurchaseVno(long ClinicId);
 
     /// <summary>
     /// Stock Issue VNO => [I1-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetStockIssueVno(long HospitalId);
+    string GetStockIssueVno(long ClinicId);
 
     /// <summary>
     /// Damange VNO => [D1-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetDamageVno(long HospitalId);
+    string GetDamageVno(long ClinicId);
 
     /// <summary>
     /// OPD Registration ID => [P-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetOPDRegId(long HospitalId);
+    string GetOPDRegId(long ClinicId);
 
     /// <summary>
     /// OPD Voucher ID => [VNo-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetOPDVNo(long HospitalId);
+    string GetOPDVNo(long ClinicId);
     
     /// <summary>
     /// Admission No ID => [ADM-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetAdmissionNo(long HospitalId);
+    string GetAdmissionNo(long ClinicId);
 
     /// <summary>
     /// Damange VNO => [D1-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetLabVoucherVno(long HospitalId);
+    string GetLabVoucherVno(long ClinicId);
 
     /// <summary>
     /// RoomBill VNO => [RVN-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetRoomBill(long HospitalId);
+    string GetRoomBill(long ClinicId);
 
     /// <summary>
     /// Pharmacy Voucher VNO => [PVN-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetPharmacyVoucherVno(long HospitalId);
+    string GetPharmacyVoucherVno(long ClinicId);
     
     /// <summary>
     /// RoomBill VNO => [AVN-010125-RXYDQC]
     /// </summary>
-    /// <param name="HospitalId"></param>
+    /// <param name="ClinicId"></param>
     /// <returns></returns>
-    string GetAdmissionVNo(long HospitalId);
+    string GetAdmissionVNo(long ClinicId);
 }
