8721891c5ba9f2dcb07643d549114b324c43b112
ypeain <yoonpyae26@gmail.com>
2025-08-28 15:10:03 +0630

    Update IRepositoryWrapper.cs

--- WOMS.Server/Interfaces/Repositories/IRepositoryWrapper.cs
+++ WOMS.Server/Interfaces/Repositories/IRepositoryWrapper.cs
@@ -10,7 +10,7 @@ namespace WOMS.Server.Interfaces.Repositories
         IAspNetRoleRepo AspNetRoles { get; }
 
         ITokenClaimRepo TokenClaims { get; }
-        IHospitalRepo Hospitals { get; }
+        IClinicRepo Clinics { get; }
         IStateRepo States { get; }
         ITownshipRepo Townships { get; }
 
