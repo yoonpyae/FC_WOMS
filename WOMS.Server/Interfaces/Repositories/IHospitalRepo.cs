7dc97522274204e797c7f328b185d85aa78996c4
ypeain <yoonpyae26@gmail.com>
2025-08-28 15:10:00 +0630

    Delete IHospitalRepo.cs

--- WOMS.Server/Interfaces/Repositories/IHospitalRepo.cs
+++
@@ -1,9 +0,0 @@
-using WOMS.Server.Entities;
-using WOMS.Server.Interfaces.Repositories.Base;
-
-namespace WOMS.Server.Interfaces.Repositories
-{
-    public interface IHospitalRepo : IRepositoryBase<Hospital>
-    {
-    }
-}
