2622d7cc96df2ee404ec96883431865db5bd5aa2
ypeain <yoonpyae26@gmail.com>
2025-08-28 15:23:36 +0630

    Create BranchRepo.cs

---
+++ WOMS.Server/Services/Repository/BranchRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class BranchRepo(WOMSDbContext context) : RepositoryBase<Branch>(context), IBranchRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
