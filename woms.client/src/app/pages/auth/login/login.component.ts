f0e6b0b18ea1629d2f385c9e0eeb3486a95c75d4
ypeain <yoonpyae26@gmail.com>
2025-08-28 16:19:17 +0630

    Update login.component.ts

--- woms.client/src/app/pages/auth/login/login.component.ts
+++ woms.client/src/app/pages/auth/login/login.component.ts
@@ -73,7 +73,7 @@ export class LoginComponent implements OnInit {
           this.setValueToSession(res);
           this.isLoading = false;
 
-          if (this.sharedService.getDefaultClinicId() != "") {
+          if (this.sharedService.getDefaultBranchId() != "") {
             this.router.navigate(['/dashboard']);
           }
           else {
