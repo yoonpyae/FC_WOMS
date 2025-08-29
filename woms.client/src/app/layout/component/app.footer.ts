9cfdd185fce6ce7525d66bd1f6bb5feb5852afc7
ypeain <yoonpyae26@gmail.com>
2025-08-29 15:01:49 +0630

    Update app.footer.ts

--- woms.client/src/app/layout/component/app.footer.ts
+++ woms.client/src/app/layout/component/app.footer.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
+        YOON PYAE EAIN by
         <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
     </div>`
 })
