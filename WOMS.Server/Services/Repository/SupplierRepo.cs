8983b527e690199a215d64c7ac4f98bd11556bb5
ypeain <yoonpyae26@gmail.com>
2025-08-31 20:38:11 +0630

    Create SupplierRepo.cs

---
+++ WOMS.Server/Services/Repository/SupplierRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class SupplierRepo(WOMSDbContext context) : RepositoryBase<Supplier>(context), ISupplierRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
