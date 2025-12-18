06f00caafdc3bfdb05fa653e3618d19e6389daf9
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:36:59 +0630

    Create stock-item.model.ts

---
+++ woms.client/src/app/core/models/stock/stock-item.model.ts
@@ -0,0 +1,15 @@
+export interface StockItemModel {
+    itemCode: string;
+    clinicId: number;
+    itemName: string;
+    chemicalName: string;
+    typeCode: number;
+    createdOn: string | null;
+    createdBy: string | null;
+    updatedOn: string | null;
+    updatedBy: string | null;
+    deletedOn: string | null;
+    deletedBy: string | null;
+    status: boolean;
+    remark: string | null;
+}
