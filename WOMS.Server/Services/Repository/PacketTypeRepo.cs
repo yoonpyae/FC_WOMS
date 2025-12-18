b7cbd1d08ceb0f69f32cdefb3dadf2d040ba132f
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:38:20 +0630

    Create PacketTypeRepo.cs

---
+++ WOMS.Server/Services/Repository/PacketTypeRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class PacketTypeRepo(WOMSDbContext context): RepositoryBase<PacketType>(context), IPacketTypeRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
