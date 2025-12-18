663f3a13df278daf985442bb9d5e766e2e28d099
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:37:05 +0630

    Create main-stock.service.ts

---
+++ woms.client/src/app/core/services/stock/main-stock.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MainStockModel } from '@core_models/stock/main-stock.model';
+import { RootModel } from '@core_models/root.model';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainStockService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get(clinicId: number): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/mainstocks?clinicId=${clinicId}`);
+  }
+
+  getByCode(code: string, clinicId: number): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/mainstocks/code?code=${code}&clinicId=${clinicId}`);
+  }
+
+  create(model: MainStockModel): Observable<RootModel> {
+    return this.httpClient.post<RootModel>(`${environment.main_url}/stock/mainstocks`, model);
+  }
+
+  update(model: MainStockModel): Observable<RootModel> {
+    return this.httpClient.put<RootModel>(`${environment.main_url}/stock/mainstocks`, model);
+  }
+
+  delete(code: string): Observable<RootModel> {
+    return this.httpClient.delete<RootModel>(`${environment.main_url}/stock/mainstocks/code?code=${code}`)
+  }
+}
