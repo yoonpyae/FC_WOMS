291de260ab6aa5f8d1d32127032a98bc5f7e9c2e
ypeain <yoonpyae26@gmail.com>
2025-12-18 18:38:31 +0630

    Create StatesController.cs

---
+++ WOMS.Server/Controllers/Master/StatesController.cs
@@ -0,0 +1,19 @@
+﻿namespace WOMS.Server.Controllers.Master;
+
+[Authorize]
+[Route("api/master/[controller]")]
+[ApiController]
+public class StatesController(
+    IRepositoryWrapper repo) : ControllerBase
+{
+    #region CRUD Operation
+    [HttpGet]
+    [EndpointSummary("List")]
+    [EndpointDescription("List all States")]
+    public async Task<IActionResult> Get()
+    {
+        return ResponseHelper.OK_Result(await repo.States.GetAsync(), null);
+    }
+
+    #endregion
+}
