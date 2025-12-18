be05ed7e6a4b394a067269605a8df2129c9d8740
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:38:29 +0630

    Create ViMainStockRepo.cs

---
+++ WOMS.Server/Services/Repository/ViMainStockRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViMainStockRepo(WOMSDbContext context): RepositoryBase<ViMainStock>(context), IViMainStockRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
