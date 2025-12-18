06e2f70c658a19ee82ae9b72c67f52abfe7fb368
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:38:17 +0630

    Create MainStockRepo.cs

---
+++ WOMS.Server/Services/Repository/MainStockRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class MainStockRepo(WOMSDbContext context) : RepositoryBase<MainStock>(context), IMainStockRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
