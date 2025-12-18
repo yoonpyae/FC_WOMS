893241c3d472014b47b9c44e53164d02f632180d
ypeain <yoonpyae26@gmail.com>
2025-12-18 18:37:42 +0630

    Create supplier.model.ts

---
+++ woms.client/src/app/core/models/master/supplier.model.ts
@@ -0,0 +1,18 @@
+export interface SupplierModel {
+    supplierId: number;
+    cliniclId: number;
+    companyName: null;
+    contactPerson: null | string;
+    address: null;
+    phone: null;
+    email: null;
+    balance: null;
+    createdOn: string;
+    createdBy: string;
+    updatedOn: null | string;
+    updatedBy: null | string;
+    deletedOn: null;
+    deletedBy: null;
+    status: null;
+    remark: null;
+}
