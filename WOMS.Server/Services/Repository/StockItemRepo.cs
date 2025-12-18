be26f66675de84e18ecb06ee15903ed6eee63095
ypeain <yoonpyae26@gmail.com>
2025-12-18 22:38:26 +0630

    Create StockItemRepo.cs

---
+++ WOMS.Server/Services/Repository/StockItemRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class StockItemRepo(WOMSDbContext context): RepositoryBase<StockItem>(context), IStockItemRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
