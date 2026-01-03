73082559614b6cab9e4df3486fca84b706c515bd
ypeain <yoonpyae26@gmail.com>
2026-01-03 21:57:25 +0630

    Create Doctor.cs

---
+++ WOMS.Server/Entities/Doctor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[PrimaryKey("DoctorId", "BranchId")]
+[Table("Doctor")]
+public partial class Doctor
+{
+    [Key]
+    public long DoctorId { get; set; }
+
+    [Key]
+    public long BranchId { get; set; }
+
+    [StringLength(200)]
+    public string? Name { get; set; }
+
+    [StringLength(500)]
+    public string? Degree { get; set; }
+
+    [StringLength(500)]
+    public string? Spalized { get; set; }
+
+    [StringLength(200)]
+    public string? Photo { get; set; }
+
+    [StringLength(200)]
+    public string? Sign { get; set; }
+
+    public double? ReferFee { get; set; }
+
+    [Column("OPDReferFee")]
+    public double? OpdreferFee { get; set; }
+
+    public double? ConsultantFee { get; set; }
+
+    [Column("ECGFee")]
+    public double? Ecgfee { get; set; }
+
+    [Column("XRayFee")]
+    public double? XrayFee { get; set; }
+
+    public double? UltrasoundFee { get; set; }
+
+    public double? RoundFee { get; set; }
+
+    [StringLength(100)]
+    public string? MonTime { get; set; }
+
+    [StringLength(100)]
+    public string? TueTime { get; set; }
+
+    [StringLength(100)]
+    public string? WedTime { get; set; }
+
+    [StringLength(100)]
+    public string? ThuTime { get; set; }
+
+    [StringLength(100)]
+    public string? FriTime { get; set; }
+
+    [StringLength(100)]
+    public string? SatTime { get; set; }
+
+    [StringLength(100)]
+    public string? SunTime { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(256)]
+    public string? DeletedBy { get; set; }
+
+    public bool Status { get; set; }
+
+    public string? Remark { get; set; }
+}
