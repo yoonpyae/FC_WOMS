843ae5ffe63399fd521cdf66147aa31d2113fb8c
ypeain <yoonpyae26@gmail.com>
2026-01-03 21:55:14 +0630

    Create doctor.model.ts

---
+++ woms.client/src/app/core/models/master/doctor.model.ts
@@ -0,0 +1,31 @@
+export interface DoctorModel {
+    doctorId: number;
+    branchId: number;
+    name: string;
+    degree: string | null;
+    spalized: string | null;
+    photo: string | null;
+    sign: string | null;
+    referFee: number | null;
+    opdreferFee: number | null;
+    consultantFee: number | null;
+    ecgfee: number | null;
+    xrayFee: number | null;
+    ultrasoundFee: number | null;
+    roundFee: number | null;
+    monTime: string | null;
+    tueTime: string | null;
+    wedTime: string | null;
+    thuTime: string | null;
+    friTime: string | null;
+    satTime: string | null;
+    sunTime: string | null;
+    createdOn: string;
+    createdBy: string;
+    updatedOn: string;
+    updatedBy: string;
+    deletedOn: string | null;
+    deletedBy: string | null;
+    status: string | null;
+    remark: string | null;
+}
