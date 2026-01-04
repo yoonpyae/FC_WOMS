841ca296440a5bc78985439f8922a36ae1f138a0
ypeain <yoonpyae26@gmail.com>
2026-01-04 23:52:52 +0630

    Create SaleDetail.cs

---
+++ WOMS.Server/Entities/SaleDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[PrimaryKey("SaleVno", "ItemCode", "TypeCode")]
+[Table("SaleDetail")]
+public partial class SaleDetail
+{
+    [Key]
+    [Column("SaleVNo")]
+    [StringLength(50)]
+    public string SaleVno { get; set; } = null!;
+
+    [Key]
+    [StringLength(5)]
+    public string ItemCode { get; set; } = null!;
+
+    [Key]
+    public long TypeCode { get; set; }
+
+    public int Qty { get; set; }
+
+    public double Price { get; set; }
+
+    public DateOnly ExpireDate { get; set; }
+}
