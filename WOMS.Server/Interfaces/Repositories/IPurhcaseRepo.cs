6a8bfbfa0574b9b4e9834e1ffc3d5d9704c82d8d
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:06:31 +0630

    Update IPurhcaseRepo.cs

--- WOMS.Server/Interfaces/Repositories/IPurhcaseRepo.cs
+++ WOMS.Server/Interfaces/Repositories/IPurhcaseRepo.cs
@@ -1,6 +1,6 @@
 ﻿namespace WOMS.Server.Interfaces.Repositories
 {
-    public interface IPurhcaseRepo:IRepositoryBase<Purchase>
+    public interface IPurhcaseRepo : IRepositoryBase<Purchase>
     {
     }
 }
