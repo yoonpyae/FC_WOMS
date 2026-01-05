aab68272a516d639aaeb9fb058c7840e8624fad8
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:26 +0630

    Create UpdatePurcahseModel.cs

---
+++ WOMS.Server/Models/Stock/Purchases/UpdatePurcahseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WOMS.Server.Models.Stock.Purchases
+{
+    public class UpdatePurcahseModel
+    {
+        public string? PurchaseVno { get; set; }
+        public double? TotalAmount { get; set; }
+        public double? DiscountAmount { get; set; }
+        public double? NetAmount { get; set; }
+        public double? PayAmount { get; set; }
+        public double? LeftAmount { get; set; }
+        public DateTime? PaidDate { get; set; }
+
+    }
+}
