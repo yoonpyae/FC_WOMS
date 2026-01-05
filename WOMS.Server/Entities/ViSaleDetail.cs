55aa2332b676b90459475004f6dbacf214109cab
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:05:03 +0630

    Create ViSaleDetail.cs

---
+++ WOMS.Server/Entities/ViSaleDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViSaleDetail
+{
+    [Column("SaleVNo")]
+    [StringLength(50)]
+    public string SaleVno { get; set; } = null!;
+
+    public long BranchId { get; set; }
+
+    [Column("ManualVNO")]
+    [StringLength(50)]
+    public string? ManualVno { get; set; }
+
+    public long? PatientId { get; set; }
+
+    [StringLength(200)]
+    public string PatientName { get; set; } = null!;
+
+    [Column(TypeName = "datetime")]
+    public DateTime? SaleDate { get; set; }
+
+    [StringLength(5)]
+    public string ItemCode { get; set; } = null!;
+
+    [StringLength(200)]
+    public string? ItemName { get; set; }
+
+    public long TypeCode { get; set; }
+
+    [StringLength(200)]
+    public string? TypeName { get; set; }
+
+    public int Qty { get; set; }
+
+    public double Price { get; set; }
+
+    public DateOnly ExpireDate { get; set; }
+}
