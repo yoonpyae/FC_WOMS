968cf56abaf98a2f2164f81db6638d6a7e245fb8
ypeain <yoonpyae26@gmail.com>
2026-01-05 19:43:29 +0630

    Create SaleEntryModel.cs

---
+++ WOMS.Server/Models/Stock/Sales/SaleEntryModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Models.Stock.Sales
+{
+    public class SaleEntryModel:Sale
+    {
+        public List<SaleEntryDetailModel> Detail { get; set; } = new List<SaleEntryDetailModel>();
+    }
+}
