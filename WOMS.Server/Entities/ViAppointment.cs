b0417b8b71039c2462d42fada53aa2a69d0c506f
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:03:49 +0630

    Create ViAppointment.cs

---
+++ WOMS.Server/Entities/ViAppointment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViAppointment
+{
+    [Column("ANo")]
+    public int Ano { get; set; }
+
+    [Column("DoctorID")]
+    public long DoctorId { get; set; }
+
+    [StringLength(200)]
+    public string? DoctorName { get; set; }
+
+    public DateOnly AppointmentDate { get; set; }
+
+    public long BranchId { get; set; }
+
+    [StringLength(200)]
+    public string Name { get; set; } = null!;
+
+    [StringLength(50)]
+    public string PhoneNo { get; set; } = null!;
+
+    [StringLength(20)]
+    public string PatientId { get; set; } = null!;
+
+    [StringLength(200)]
+    public string PatientName { get; set; } = null!;
+
+    [StringLength(50)]
+    public string PatientPhoneNo { get; set; } = null!;
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(256)]
+    public string? DeletedBy { get; set; }
+
+    public bool Status { get; set; }
+
+    public string? Remark { get; set; }
+}
