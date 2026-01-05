5000881619076a6f7b683fae8c961503eab50f2a
ypeain <yoonpyae26@gmail.com>
2026-01-05 16:43:53 +0630

    Create PatientRepo.cs

---
+++ WOMS.Server/Services/Repository/PatientRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class PatientRepo(WOMSDbContext context) : RepositoryBase<Patient>(context), IPatientRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
