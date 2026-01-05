906805efb43a8fdd2f511f25a4c6c0dd273a13db
ypeain <yoonpyae26@gmail.com>
2026-01-05 16:12:34 +0630

    Create purchase-entry-detail.model.ts

---
+++ woms.client/src/app/core/models/stock/purchase/purchase-entry-detail.model.ts
@@ -0,0 +1,18 @@
+export interface PurchaseEntryDetailModel {
+    purchaseVno?: string;
+    itemCode: string;
+    typeCode: number;
+    qty: number;
+    price: number;
+    totalQty: number; //should be totalPrice
+    expireDate: string | null;
+    saleQty: null;
+    salePrice: number;
+}
+
+export interface PurchaseEntryDisplayModel extends PurchaseEntryDetailModel {
+    itemName: string;
+    typeName: string;
+    capacity: number;
+    salePrice: number;
+}
