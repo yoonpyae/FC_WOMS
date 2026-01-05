6617a04c60d5e1354195f1f26127d07229036f9c
ypeain <yoonpyae26@gmail.com>
2026-01-05 21:00:58 +0630

    Create entry.component.ts

---
+++ woms.client/src/app/pages/stock/sale/entry/entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sale-entry',
+  imports: [],
+  templateUrl: './entry.component.html'
+})
+export class EntryComponent {
+
+}
