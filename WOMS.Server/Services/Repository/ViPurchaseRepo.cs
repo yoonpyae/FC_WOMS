39c0ed176b993ae3384453198cb1234cff1c4cd9
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:47 +0630

    Create ViPurchaseRepo.cs

---
+++ WOMS.Server/Services/Repository/ViPurchaseRepo.cs
@@ -0,0 +1,8 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViPurchaseRepo(WOMSDbContext context) : RepositoryBase<ViPurchase>(context), IViPurchaseRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    
+    }
+}
