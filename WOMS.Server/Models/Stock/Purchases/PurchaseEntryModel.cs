7608f32af60edc13fb2cd2ebc662b21f786f7e9c
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:20 +0630

    Create PurchaseEntryModel.cs

---
+++ WOMS.Server/Models/Stock/Purchases/PurchaseEntryModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Models.Stock.Purchases
+{
+    public class PurchaseEntryModel : Purchase
+    {
+        public List<PurchaseEntryDetailModel> Detail { get; set; } = new List<PurchaseEntryDetailModel>();
+    }
+}
