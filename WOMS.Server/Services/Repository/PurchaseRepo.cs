cec1d07b53d8293f3b7b19f67c479d06f85a5efb
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:32 +0630

    Create PurchaseRepo.cs

---
+++ WOMS.Server/Services/Repository/PurchaseRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class PurchaseRepo(WOMSDbContext context) : RepositoryBase<Purchase>(context), IPurchaseRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
