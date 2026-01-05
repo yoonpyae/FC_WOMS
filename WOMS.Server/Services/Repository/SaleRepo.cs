ce8e0b76b35e15781f18c533c1ddb48c8fd741a7
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:42 +0630

    Create SaleRepo.cs

---
+++ WOMS.Server/Services/Repository/SaleRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class SaleRepo(WOMSDbContext context) : RepositoryBase<Sale>(context), ISaleRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
