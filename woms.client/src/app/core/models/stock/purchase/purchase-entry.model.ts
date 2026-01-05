33c8018ca04836ea41863babc6f2ce763ca8d57b
ypeain <yoonpyae26@gmail.com>
2026-01-05 16:12:38 +0630

    Create purchase-entry.model.ts

---
+++ woms.client/src/app/core/models/stock/purchase/purchase-entry.model.ts
@@ -0,0 +1,26 @@
+import { PurchaseEntryDetailModel } from "./purchase-entry-detail.model";
+
+export interface PurchaseEntryModel {
+    purchaseVno: string;
+    branchId: number;
+    manualVno: string;
+    supplierId: number;
+    purchaseDate: string | Date | null;
+    totalAmount: number;
+    discountAmount: number;
+    netAmount: number;
+    payAmount: number;
+    leftAmount: number;
+    paymentType: string;
+    paidDate: string | null;
+    createdOn: string | null;
+    createdBy: string | null;
+    updatedOn: string | null;
+    updatedBy: string | null;
+    deletedOn: string | null;
+    deletedBy: string | null;
+    status: boolean;
+    remark: string | null;
+    detail: PurchaseEntryDetailModel[];
+}
+
