07597a4c72b07fa1d655141b69cdd77f939fb2aa
ypeain <yoonpyae26@gmail.com>
2026-01-05 16:44:09 +0630

    Create ViSaleRepo.cs

---
+++ WOMS.Server/Services/Repository/ViSaleRepo.cs
@@ -0,0 +1,8 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViSaleRepo(WOMSDbContext context) : RepositoryBase<ViSale>(context), IViSaleRepo
+    {
+        private readonly WOMSDbContext _context = context;
+
+    }
+}
