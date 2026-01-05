38075962dab11e8e4bdc7366d329e7ca3189e458
ypeain <yoonpyae26@gmail.com>
2026-01-05 15:39:23 +0630

    Create PurchaseListModel.cs

---
+++ WOMS.Server/Models/Stock/Purchases/PurchaseListModel.cs
@@ -0,0 +1,37 @@
+﻿namespace WOMS.Server.Models.Stock.Purchases
+{
+    public class PurchaseListModel : ViPurchase
+    {
+        public List<ViPurchaseDetail> Details { get; set; } = null!;
+
+        public PurchaseListModel ConstructFromView(ViPurchase purchase, List<ViPurchaseDetail> purchaseDetails)
+        {
+            return new PurchaseListModel()
+            {
+                PurchaseVno = purchase.PurchaseVno,
+                BranchId = purchase.BranchId,
+                ManualVno = purchase.ManualVno,
+                SupplierId = purchase.SupplierId,
+                SupplierCompanyName = purchase.SupplierCompanyName,
+                SupplierName = purchase.SupplierName,
+                TotalAmount = purchase.TotalAmount,
+                DiscountAmount = purchase.DiscountAmount,
+                NetAmount = purchase.NetAmount,
+                PayAmount = purchase.PayAmount,
+                LeftAmount = purchase.LeftAmount,
+                PaymentType = purchase.PaymentType,
+                PaidDate = purchase.PaidDate,
+                PurchaseDate = purchase.PurchaseDate,
+                CreatedBy = purchase.CreatedBy,
+                CreatedOn = purchase.CreatedOn,
+                UpdatedBy = purchase.UpdatedBy,
+                UpdatedOn = purchase.UpdatedOn,
+                DeletedBy = purchase.DeletedBy,
+                DeletedOn = purchase.DeletedOn,
+                Status = purchase.Status,
+                Remark = purchase.Remark,
+                Details = purchaseDetails
+            };
+        }
+    }
+}
