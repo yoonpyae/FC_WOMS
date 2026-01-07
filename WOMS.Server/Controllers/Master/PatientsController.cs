df9af939e08e2ee8a8a6f16592f4481f5a258e9f
ypeain <yoonpyae26@gmail.com>
2026-01-07 12:26:37 +0630

    Create PatientsController.cs

---
+++ WOMS.Server/Controllers/Master/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WOMS.Server.Controllers.Master;
+
+[Authorize]
+[Route("api/master/[controller]")]
+[ApiController]
+public class PatientsController(IRepositoryWrapper repo) : ControllerBase
+{
+    #region CRUD Operation
+    [HttpGet]
+    [EndpointSummary("List")]
+    [EndpointDescription("List all Patient without deleted data")]
+    public async Task<IActionResult> Get(long branchId)
+            => ResponseHelper.OK_Result(
+               await repo.Patients.GetAsync(x => !x.DeletedOn.HasValue && x.BranchId == branchId),
+               null);
+
+    [HttpGet("{id:long}")]
+    [EndpointSummary("Get By Id")]
+    [EndpointDescription("Get Patient by Id")]
+    public async Task<IActionResult> Get(long id, long branchId)
+            => ResponseHelper.OK_Result(
+        await repo.Patients.GetFirstAsync(x => x.PatientId == id && x.BranchId == branchId) ,
+        null);
+
+    #endregion
+}
