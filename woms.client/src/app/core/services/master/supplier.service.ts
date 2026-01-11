d5ee025fe39416da1d827f2f56ab4cdfcab78d7b
ypeain <yoonpyae26@gmail.com>
2026-01-11 21:12:35 +0630

    Update supplier.service.ts

--- woms.client/src/app/core/services/master/supplier.service.ts
+++ woms.client/src/app/core/services/master/supplier.service.ts
@@ -14,26 +14,26 @@ export class SupplierService {
   }
 
   get(branchId: number): Observable<RootModel> {
-    return this.httpClient.get<RootModel>(`${environment.main_url}/master/supplier?branchId=${branchId}`);
+    return this.httpClient.get<RootModel>(`${environment.main_url}/master/suppliers?branchId=${branchId}`);
   }
 
   getById(id: number, branchId: number): Observable<RootModel> {
-    return this.httpClient.get<RootModel>(`${environment.main_url}/master/supplier/${id}?branchId=${branchId}`);
+    return this.httpClient.get<RootModel>(`${environment.main_url}/master/suppliers/${id}?branchId=${branchId}`);
   }
 
   getAutoId(branchId: number): Observable<RootModel> {
-    return this.httpClient.get<RootModel>(`${environment.main_url}/master/supplier/auto-id?branchId=${branchId}`);
+    return this.httpClient.get<RootModel>(`${environment.main_url}/master/suppliers/auto-id?branchId=${branchId}`);
   }
 
   create(model: SupplierModel) {
-    return this.httpClient.post<RootModel>(`${environment.main_url}/master/supplier`, model);
+    return this.httpClient.post<RootModel>(`${environment.main_url}/master/suppliers`, model);
   }
 
   update(model: SupplierModel) {
-    return this.httpClient.put<RootModel>(`${environment.main_url}/master/supplier`, model);
+    return this.httpClient.put<RootModel>(`${environment.main_url}/master/suppliers`, model);
   }
 
   delete(id: number) {
-    return this.httpClient.delete<RootModel>(`${environment.main_url}/master/supplier/${id}`, {});
+    return this.httpClient.delete<RootModel>(`${environment.main_url}/master/suppliers/${id}`, {});
   }
 }
