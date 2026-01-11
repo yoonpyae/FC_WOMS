9df2cd5ae3c6635e8fbee8c6098cd096b70d5036
ypeain <yoonpyae26@gmail.com>
2026-01-11 21:11:32 +0630

    Create patient.model.ts

---
+++ woms.client/src/app/core/models/master/patient.model.ts
@@ -0,0 +1,46 @@
+export interface PatientModel {
+  patientId: string;
+  branchId: number;
+  name: string;
+  nrc: string;
+  dob: string;
+  age: string;
+  stateId: number;
+  townshipId: number;
+  addressDetail: string;
+  phone: string;
+  doctorId: number;
+  createdOn: string | null;
+  createdBy: string | null;
+  updatedOn: string | null;
+  updatedBy: string | null;
+  deletedOn: string | null;
+  deletedBy: string | null;
+  status: boolean;
+  remark: null;
+}
+
+export interface ViPatientModel {
+  patientId: string;
+  branchId: number;
+  patientName: string;
+  nrc: string;
+  dob: string;
+  age: string;
+  doctorId: number;
+  doctorName: string;
+  stateId: number;
+  stateName: string;
+  townshipId: number;
+  townshipName: string;
+  addressDetail: string;
+  phone: string;
+  createdOn: string | null;
+  createdBy: string | null;
+  updatedOn: string | null;
+  updatedBy: string | null;
+  deletedOn: string | null;
+  deletedBy: string | null;
+  status: boolean;
+  remark: null;
+}
