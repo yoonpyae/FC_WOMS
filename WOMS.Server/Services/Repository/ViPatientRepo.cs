a4045045b373eb180c217594ff316e235898c9b2
ypeain <yoonpyae26@gmail.com>
2026-01-11 21:13:41 +0630

    Create ViPatientRepo.cs

---
+++ WOMS.Server/Services/Repository/ViPatientRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViPatientRepo(WOMSDbContext context) : RepositoryBase<ViPatient>(context), IViPatientRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
