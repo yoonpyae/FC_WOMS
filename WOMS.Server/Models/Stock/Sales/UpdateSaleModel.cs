dfdd7a2be15d5c3e0ddc4f14c41256a9176d8815
ypeain <yoonpyae26@gmail.com>
2026-01-12 16:58:40 +0630

    Create UpdateSaleModel.cs

---
+++ WOMS.Server/Models/Stock/Sales/UpdateSaleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WOMS.Server.Models.Stock.Sales
+{
+    public class UpdateSaleModel
+    {
+        public string? SaleVno { get; set; }
+        public double? PaidAmount { get; set; }
+        public double? LeftAmount { get; set; }
+    }
+}
