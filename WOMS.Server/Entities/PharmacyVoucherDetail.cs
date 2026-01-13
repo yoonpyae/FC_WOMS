0c2ca49bf9303c80122c2acca529dd889e69a6a7
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:18:31 +0630

    Create PharmacyVoucherDetail.cs

---
+++ WOMS.Server/Entities/PharmacyVoucherDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[PrimaryKey("Vno", "ItemCode", "TypeCode")]
+[Table("PharmacyVoucherDetail")]
+public partial class PharmacyVoucherDetail
+{
+    [Key]
+    [Column("VNo")]
+    [StringLength(50)]
+    public string Vno { get; set; } = null!;
+
+    [Key]
+    [StringLength(5)]
+    public string ItemCode { get; set; } = null!;
+
+    [Key]
+    public long TypeCode { get; set; }
+
+    public int Qty { get; set; }
+
+    public double Price { get; set; }
+
+    public double Amount { get; set; }
+}
