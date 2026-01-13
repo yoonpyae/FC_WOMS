3a4d6b1c920a2897912db751eb2ceb616e7eb9f8
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:28:29 +0630

    Update PharmacyVouchersController.cs

--- WOMS.Server/Controllers/Pharmacy/PharmacyVouchersController.cs
+++ WOMS.Server/Controllers/Pharmacy/PharmacyVouchersController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WOMS.Server.Controllers.Pharmacy
+namespace WOMS.Server.Controllers.Pharmacy;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PharmacyVouchersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PharmacyVouchersController : ControllerBase
-    {
-    }
 }
