f2033823e4933a282713e7079b02c3cc32d0897e
ypeain <yoonpyae26@gmail.com>
2026-01-13 23:04:26 +0630

    Update main-stock.service.ts

--- woms.client/src/app/core/services/stock/main-stock.service.ts
+++ woms.client/src/app/core/services/stock/main-stock.service.ts
@@ -23,7 +23,7 @@ export class MainStockService {
   }
 
   getByActive(branchId: number): Observable<RootModel> {
-    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/pharmacystocks/active?branchId=${branchId}`);
+    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/mainstocks/active?branchId=${branchId}`);
   }
 
   create(model: MainStockModel): Observable<RootModel> {
