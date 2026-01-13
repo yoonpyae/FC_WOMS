bace843214f0ef68f923b05949615897300f6817
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:20:01 +0630

    Delete ViSaleRepo.cs

--- WOMS.Server/Services/Repository/ViSaleRepo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WOMS.Server.Services.Repository
-{
-    public class ViSaleRepo(WOMSDbContext context) : RepositoryBase<ViSale>(context), IViSaleRepo
-    {
-        private readonly WOMSDbContext _context = context;
-
-    }
-}
