f9e20aa1e02e99757c6fd91f79197319815be55c
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:19:24 +0630

    Create UpdatePharmacyVoucherModel.cs

---
+++ WOMS.Server/Models/Pharmacy/UpdatePharmacyVoucherModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WOMS.Server.Models.PharmacyVouchers
+{
+    public class UpdatePharmacyVoucherModel
+    {
+        public string? Vno { get; set; }
+        public double? PaidAmount { get; set; }
+        public double? LeftAmount { get; set; }
+    }
+}
