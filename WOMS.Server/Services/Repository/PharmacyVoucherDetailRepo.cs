74f01cc8c4c65df4855002cebb7123db56902027
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:19:38 +0630

    Create PharmacyVoucherDetailRepo.cs

---
+++ WOMS.Server/Services/Repository/PharmacyVoucherDetailRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class PharmacyVoucherDetailRepo(WOMSDbContext context) : RepositoryBase<PharmacyVoucherDetail>(context), IPharmacyVoucherDetailRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
