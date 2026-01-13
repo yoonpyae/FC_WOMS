f71d6dbfdcfd06ec5919b9609082585583876ffe
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:23:39 +0630

    Update IPharmacyVoucherRepo.cs

--- WOMS.Server/Interfaces/Repositories/IPharmacyVoucherRepo.cs
+++ WOMS.Server/Interfaces/Repositories/IPharmacyVoucherRepo.cs
@@ -1,6 +1,6 @@
 ﻿namespace WOMS.Server.Interfaces.Repositories
 {
-    public interface IPharmacyVoucherRepo : IRepositoryBase<Sale>
+    public interface IPharmacyVoucherRepo : IRepositoryBase<PharmacyVoucher>
     {
     }
 }
