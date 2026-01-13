5674151a8238b7b15ff2a68b27c713d332ea86f6
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:18:18 +0630

    Create PharmacyVouchersController.cs

---
+++ WOMS.Server/Controllers/Pharmacy/PharmacyVouchersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WOMS.Server.Controllers.Pharmacy
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PharmacyVouchersController : ControllerBase
+    {
+    }
+}
