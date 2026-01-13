fc82f67348f7f058892275fd525f1d06fd1d3f10
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:19:19 +0630

    Create PharmacyVoucherEntryModel.cs

---
+++ WOMS.Server/Models/Pharmacy/PharmacyVoucherEntryModel.cs
@@ -0,0 +1,9 @@
+﻿using WOMS.Server.Entities;
+
+namespace WOMS.Server.Models.PharmacyVouchers
+{
+    public class PharmacyVoucherEntryModel:PharmacyVoucher
+    {
+        public List<PharmacyVoucherDetail> Details { get; set; } = [];
+    }
+}
