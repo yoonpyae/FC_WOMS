1bc572532ae07757f6cd7ea7820c0b698e79f6cb
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:18:40 +0630

    Create ViPharmacyVoucherDetail.cs

---
+++ WOMS.Server/Entities/ViPharmacyVoucherDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViPharmacyVoucherDetail
+{
+    [Column("VNo")]
+    [StringLength(50)]
+    public string Vno { get; set; } = null!;
+
+    [StringLength(5)]
+    public string ItemCode { get; set; } = null!;
+
+    [StringLength(200)]
+    public string? ItemName { get; set; }
+
+    public long TypeCode { get; set; }
+
+    [StringLength(200)]
+    public string? TypeName { get; set; }
+
+    public int Qty { get; set; }
+
+    public double Price { get; set; }
+
+    public double Amount { get; set; }
+
+    [StringLength(500)]
+    public string? ChemicalName { get; set; }
+
+    public int? Capacity { get; set; }
+}
