bb45f1782d3387a610b8e55e1fd6af4a732237ee
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:19:46 +0630

    Delete SaleDetailRepo.cs

--- WOMS.Server/Services/Repository/SaleDetailRepo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WOMS.Server.Services.Repository
-{
-    public class SaleDetailRepo(WOMSDbContext context) : RepositoryBase<SaleDetail>(context), ISaleDetailRepo
-    {
-        private readonly WOMSDbContext _context = context;
-    }
-}
