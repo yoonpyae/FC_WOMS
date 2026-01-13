a883b134ca5ef07301bc2ffe4a3cc9c9ffa3f633
ypeain <yoonpyae26@gmail.com>
2026-01-13 21:19:52 +0630

    Create ViPharmacyVoucherDetailRepo.cs

---
+++ WOMS.Server/Services/Repository/ViPharmacyVoucherDetailRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViPharmacyVoucherDetailRepo(WOMSDbContext context) : RepositoryBase<ViPharmacyVoucherDetail>(context), IViPharmacyVoucherDetailRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
