c3226b7f90f378fe86723ca1ee0130412f3fe3e3
ypeain <yoonpyae26@gmail.com>
2026-02-01 15:18:33 +0630

    Create ViAppointmentRepo.cs

---
+++ WOMS.Server/Services/Repository/ViAppointmentRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViAppointmentRepo(WOMSDbContext context) : RepositoryBase<ViAppointment>(context), IViAppointmentRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
