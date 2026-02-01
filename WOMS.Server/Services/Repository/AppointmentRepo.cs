f5819c53a281769a4f46bc6394393819544c841c
ypeain <yoonpyae26@gmail.com>
2026-02-01 15:18:18 +0630

    Create AppointmentRepo.cs

---
+++ WOMS.Server/Services/Repository/AppointmentRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class AppointmentRepo(WOMSDbContext context) : RepositoryBase<Appointment>(context), IAppointmentRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
