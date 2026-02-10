2dec93dc3ab70bb9be8d15f0125eed64db382766
ypeain <yoonpyae26@gmail.com>
2026-02-10 14:56:28 +0630

    Create ViSupplierRepo.cs

---
+++ WOMS.Server/Services/Repository/ViSupplierRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViSupplierRepo(WOMSDbContext context) : RepositoryBase<ViSupplier>(context), IVISupplierRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
