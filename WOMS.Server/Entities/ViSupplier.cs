e2bdb46b46867effdf550222c165f50495ca3b4e
ypeain <yoonpyae26@gmail.com>
2026-02-10 14:55:33 +0630

    Create ViSupplier.cs

---
+++ WOMS.Server/Entities/ViSupplier.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViSupplier
+{
+    [StringLength(200)]
+    public string? StateName { get; set; }
+
+    [Column("SupplierID")]
+    public long SupplierId { get; set; }
+
+    public long BranchId { get; set; }
+
+    [StringLength(200)]
+    public string? CompanyName { get; set; }
+
+    [StringLength(200)]
+    public string? ContactPerson { get; set; }
+
+    [StringLength(500)]
+    public string? Address { get; set; }
+
+    [StringLength(50)]
+    public string? Phone { get; set; }
+
+    public int? StateId { get; set; }
+
+    public int? TownshipId { get; set; }
+
+    [StringLength(50)]
+    public string? Email { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(256)]
+    public string? DeletedBy { get; set; }
+
+    public bool Status { get; set; }
+
+    public string? Remark { get; set; }
+
+    [StringLength(200)]
+    public string? TownshipName { get; set; }
+}
