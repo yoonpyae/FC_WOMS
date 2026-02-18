6cf13a48d67e3776ed8fbc55ecf117717f0898e9
ypeain <yoonpyae26@gmail.com>
2026-02-18 15:02:48 +0630

    Update environment.prod.ts

--- woms.client/src/environments/environment.prod.ts
+++ woms.client/src/environments/environment.prod.ts
@@ -2,9 +2,6 @@ export const environment = {
   production: true,
   docker: false,
 
-  main_url: 'https://his.esoftmm.com/api',
-  web_url: 'https://analytics.quickfoodmm.com',
-
   debug: false,
 
   default_pagination: [10, 20, 30, 50, 100],
