d2c52d9334c479f84d52d84a3dc5ed1c3095885c
ypeain <yoonpyae26@gmail.com>
2026-02-18 15:06:14 +0630

    Delete ClinicRepo.cs

--- WOMS.Server/Services/Repository/ClinicRepo.cs
+++
@@ -1,7 +0,0 @@
-namespace WOMS.Server.Services.Repository
-{
-    public class ClinicRepo(WOMSDbContext context) : RepositoryBase<Clinic>(context), IClinicRepo
-    {
-        private readonly WOMSDbContext _context = context;
-    }
-}
