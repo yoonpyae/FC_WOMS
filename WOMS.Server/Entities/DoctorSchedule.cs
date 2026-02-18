ec6bcd651e1be7cde2f7af1e0afa0266d8488f03
ypeain <yoonpyae26@gmail.com>
2026-02-18 15:04:20 +0630

    Create DoctorSchedule.cs

---
+++ WOMS.Server/Entities/DoctorSchedule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[PrimaryKey("ScheduleId", "DoctorId", "BranchId")]
+[Table("DoctorSchedule")]
+public partial class DoctorSchedule
+{
+    [Key]
+    public long ScheduleId { get; set; }
+
+    [Key]
+    public long DoctorId { get; set; }
+
+    [Key]
+    public long BranchId { get; set; }
+
+    [StringLength(50)]
+    public string? DayOfWeek { get; set; }
+
+    public TimeOnly? StartTime { get; set; }
+
+    public TimeOnly? EndTime { get; set; }
+
+    public long? MaxPatient { get; set; }
+
+    public bool? Status { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(256)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(256)]
+    public string? DeletedBy { get; set; }
+}
