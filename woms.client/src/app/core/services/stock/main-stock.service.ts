41a48c18317b2bee4e7b51e272b998ba56eee2b4
ypeain <yoonpyae26@gmail.com>
2026-02-19 16:30:05 +0630

    Update main-stock.service.ts

--- woms.client/src/app/core/services/stock/main-stock.service.ts
+++ woms.client/src/app/core/services/stock/main-stock.service.ts
@@ -12,14 +12,14 @@ export class MainStockService {
 
   constructor(private httpClient: HttpClient) { }
 
-  get(clinicId: number, branchId: number): Observable<RootModel> {
+  get(branchId: number): Observable<RootModel> {
     return this.httpClient.get<RootModel>(
-      `${environment.main_url}/stock/mainstocks?clinicId=${clinicId}&branchId=${branchId}`
+      `${environment.main_url}/stock/mainstocks?branchId=${branchId}`
     );
   }
 
-  getByCode(code: string, clinicId: number): Observable<RootModel> {
-    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/mainstocks/code?code=${code}&clinicId=${clinicId}`);
+  getByCode(code: string): Observable<RootModel> {
+    return this.httpClient.get<RootModel>(`${environment.main_url}/stock/mainstocks/code?code=${code}`);
   }
 
   getByActive(branchId: number): Observable<RootModel> {
