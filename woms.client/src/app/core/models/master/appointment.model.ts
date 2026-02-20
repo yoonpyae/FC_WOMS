4da14086d079095c564ad209cd38fd3ae0258bc2
ypeain <yoonpyae26@gmail.com>
2026-02-20 17:11:08 +0630

    Update appointment.model.ts

--- woms.client/src/app/core/models/master/appointment.model.ts
+++ woms.client/src/app/core/models/master/appointment.model.ts
@@ -1,18 +1,38 @@
 export interface AppointmentModel {
   ano: number;
-  doctorId: number;
-  doctorName: string;
+  scheduleId: number;
+  patientId: string;
   appointmentDate: string | Date;
   branchId: number;
-  patientId: string;
-  name: string;
-  phoneNo: string;
   createdOn: string | null;
   createdBy: string | null;
   updatedOn: string | null;
   updatedBy: string | null;
   deletedOn: string | null;
   deletedBy: string | null;
-  status: boolean;
+  status: string;
   remark: null;
+}
+
+export interface ViAppointmentModel {
+  ano: number;
+  branchId: number;
+  appointmentDate: string;
+  appointmentStatus: string;
+  appointmentRemark: null | string;
+  patientName: null | string;
+  patientPhone: null | string;
+  dayOfWeek: null | string;
+  startTime: null | string;
+  endTime: null | string;
+  doctorName: null | string;
+  specialized: null | string;
+  scheduleId: number;
+  patientId: null | string;
+  createdOn: string | null;
+  createdBy: string | null;
+  updatedOn: string | null;
+  updatedBy: string | null;
+  deletedOn: string | null;
+  deletedBy: string | null;
 }
