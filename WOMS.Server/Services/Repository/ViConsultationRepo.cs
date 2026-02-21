ebc13edbe93bff63044b5d4a7c0681d3e7ebe13c
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:03:05 +0630

    Create ViConsultationRepo.cs

---
+++ WOMS.Server/Services/Repository/ViConsultationRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViConsultationRepo(WOMSDbContext context) : RepositoryBase<ViConsultation>(context), IViConsultationRepo
+    {
+            private readonly WOMSDbContext _context = context;
+    }
+}
