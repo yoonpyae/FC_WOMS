bb2bb7eb72fb3f906e3357bd567db6f8bfda2d9d
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:02:33 +0630

    Create ConsultationRepo.cs

---
+++ WOMS.Server/Services/Repository/ConsultationRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ConsultationRepo(WOMSDbContext context) : RepositoryBase<Consultation>(context), IConsultationRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
