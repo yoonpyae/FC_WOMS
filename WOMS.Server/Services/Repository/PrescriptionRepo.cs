8a7773da3fef1b6b13c3d0d6b4a45b54e0c515ed
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:03:07 +0630

    Create PrescriptionRepo.cs

---
+++ WOMS.Server/Services/Repository/PrescriptionRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class PrescriptionRepo(WOMSDbContext context) : RepositoryBase<Prescription>(context), IPrescriptionRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
