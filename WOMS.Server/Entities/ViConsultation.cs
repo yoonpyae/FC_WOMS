167d16c845a278cc8884848f3f14b1dc963efc0f
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:02:22 +0630

    Create ViConsultation.cs

---
+++ WOMS.Server/Entities/ViConsultation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViConsultation
+{
+    [StringLength(20)]
+    public string ConsultationId { get; set; } = null!;
+
+    public long BranchId { get; set; }
+
+    public long DoctorId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime VisitDate { get; set; }
+
+    [StringLength(20)]
+    public string? PatientId { get; set; }
+
+    [Column("ANo")]
+    public long? Ano { get; set; }
+
+    public string? Symptoms { get; set; }
+
+    public string? Diagnosis { get; set; }
+
+    public string? Notes { get; set; }
+
+    [StringLength(20)]
+    public string Status { get; set; } = null!;
+
+    [Column(TypeName = "datetime")]
+    public DateTime? CreatedOn { get; set; }
+
+    [StringLength(50)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(50)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(50)]
+    public string? DeletedBy { get; set; }
+
+    [StringLength(200)]
+    public string? DoctorName { get; set; }
+
+    [StringLength(200)]
+    public string PatientName { get; set; } = null!;
+}
