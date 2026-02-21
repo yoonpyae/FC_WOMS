b28d92895355f7af54e3e6d5b24b5c135f2292fb
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:02:27 +0630

    Create ViPrescription.cs

---
+++ WOMS.Server/Entities/ViPrescription.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WOMS.Server.Entities;
+
+[Keyless]
+public partial class ViPrescription
+{
+    public long PrescriptionId { get; set; }
+
+    [StringLength(20)]
+    public string ConsultationId { get; set; } = null!;
+
+    public long BranchId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? Date { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreatedOn { get; set; }
+
+    [StringLength(50)]
+    public string? CreatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? UpdatedOn { get; set; }
+
+    [StringLength(50)]
+    public string? UpdatedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime? DeletedOn { get; set; }
+
+    [StringLength(50)]
+    public string? DeletedBy { get; set; }
+
+    public bool Status { get; set; }
+
+    [StringLength(200)]
+    public string? DoctorName { get; set; }
+
+    public long DoctorId { get; set; }
+}
