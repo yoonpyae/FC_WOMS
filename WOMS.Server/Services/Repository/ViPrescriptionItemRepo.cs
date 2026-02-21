bf82f1dca20698459c754af60189db88a37250e8
ypeain <yoonpyae26@gmail.com>
2026-02-21 22:03:02 +0630

    Create ViPrescriptionItemRepo.cs

---
+++ WOMS.Server/Services/Repository/ViPrescriptionItemRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ViPrescriptionItemRepo(WOMSDbContext context) : RepositoryBase<ViPrescriptionItem>(context), IViPrescriptionItemRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
