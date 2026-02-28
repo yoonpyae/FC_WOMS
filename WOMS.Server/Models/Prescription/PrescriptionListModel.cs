80a54e55cdc25d72d2f94419259c43a2790e2cf0
ypeain <yoonpyae26@gmail.com>
2026-02-28 20:17:37 +0630

    Delete PrescriptionListModel.cs

--- WOMS.Server/Models/Prescription/PrescriptionListModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WOMS.Server.Models.Prescription
-{
-    public class PrescriptionListModel:ViPrescription
-    {
-        public List<ViPrescriptionItem> Details { get; set; } = null!;
-    }
-}
