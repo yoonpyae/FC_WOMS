985cabc80b2dfdbcbaf223daefcd591bc783235c
ypeain <yoonpyae26@gmail.com>
2026-03-03 16:40:59 +0630

    Create consultation.model.ts

---
+++ woms.client/src/app/core/models/master/consultation.model.ts
@@ -0,0 +1,39 @@
+export interface ConsultationModel {
+    consultationId: string;
+    branchId: number;
+    doctorId: number;
+    visitDate: string;
+    patientId: string;
+    ano: number | null;
+    symptoms: string | null;
+    diagnosis: string | null;
+    notes: string | null;
+    status: string;
+    createdOn: string | null;
+    createdBy: string | null;
+    updatedOn: string | null;
+    updatedBy: string | null;
+    deletedOn: string | null;
+    deletedBy: string | null;
+}
+
+export interface ViConsultationModel {
+    consultationId: string;
+    branchId: number;
+    doctorId: number;
+    visitDate: string;
+    patientId: string;
+    ano: number | null;
+    symptoms: string | null;
+    diagnosis: string | null;
+    notes: string | null;
+    status: string;
+    createdOn: string | null;
+    createdBy: string | null;
+    updatedOn: string | null;
+    updatedBy: string | null;
+    deletedOn: string | null;
+    deletedBy: string | null;
+    doctorName: string;
+    patientName: string;
+}
