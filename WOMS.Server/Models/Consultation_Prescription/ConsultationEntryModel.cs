633b4b5b90c05ebe8b25b59ca8095c512e5eafc2
ypeain <yoonpyae26@gmail.com>
2026-03-04 16:17:00 +0630

    Create ConsultationEntryModel.cs

---
+++ WOMS.Server/Models/Consultation_Prescription/ConsultationEntryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WOMS.Server.Models.Consultation_Prescription
+{
+    public class ConsultationEntryModel
+    {
+        public Consultation Consultation { get; set; } = null!;
+        public List<Prescription> Prescriptions { get; set; } = [];
+    }
+}
