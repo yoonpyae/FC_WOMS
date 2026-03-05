197f8cfba4529d7ee226dbc254507ac3880683aa
ypeain <yoonpyae26@gmail.com>
2026-03-05 14:37:08 +0630

    Create ServiceRepo.cs

---
+++ WOMS.Server/Services/Repository/ServiceRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class ServiceRepo(WOMSDbContext context): RepositoryBase<Service>(context), IServiceRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
