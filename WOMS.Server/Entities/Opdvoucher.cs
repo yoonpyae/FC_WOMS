328a9d8352071677ef17427586cc902884111a6c
ypeain <yoonpyae26@gmail.com>
2026-03-05 16:55:06 +0630

    Update Opdvoucher.cs

--- WOMS.Server/Entities/Opdvoucher.cs
+++ WOMS.Server/Entities/Opdvoucher.cs
@@ -19,9 +19,6 @@ public partial class OPDVoucher
     public long BranchId { get; set; }
 
     [StringLength(20)]
-    public string? ConsultationId { get; set; }
-
-    [StringLength(20)]
     public string? PatientId { get; set; }
 
     [Column("VDate", TypeName = "datetime")]
