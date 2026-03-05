02188a9a5e09a98a303c77832fdb4680b8971fa2
ypeain <yoonpyae26@gmail.com>
2026-03-05 14:36:50 +0630

    Create OPDVoucherItemRepo.cs

---
+++ WOMS.Server/Services/Repository/OPDVoucherItemRepo.cs
@@ -0,0 +1,7 @@
+﻿namespace WOMS.Server.Services.Repository
+{
+    public class OPDVoucherItemRepo(WOMSDbContext context) : RepositoryBase<OPDVoucherItem>(context), IOPDVoucherItemRepo
+    {
+        private readonly WOMSDbContext _context = context;
+    }
+}
