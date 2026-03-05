82859249f5daa7f795deb961ec63798746a8e24d
ypeain <yoonpyae26@gmail.com>
2026-03-05 14:35:41 +0630

    Create ServicesController.cs

---
+++ WOMS.Server/Controllers/Master/ServicesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WOMS.Server.Controllers.Master
+{
+    [Authorize]
+    [Route("api/master/[controller]")]
+    [ApiController]
+    public class ServicesController(IRepositoryWrapper repo) : ControllerBase
+    {
+        #region CRUD Operations
+
+        //[HttpGet]
+        //[EndpointSummary("List All")]
+        //[EndpointDescription("Lists all services without deleted data.")]
+        //public async Task<IActionResult> Get(long branchId)
+        //    => ResponseHelper.OK_Result(
+        //        await repo.Services.GetAsync(x => !x.DeletedOn.HasValue && x.BranchId == branchId), null);
+        #endregion
+    }
+}
