3a715c42a364cf27e6f2b36dc2c7695eeb062b49
ypeain <yoonpyae26@gmail.com>
2026-03-06 15:21:12 +0630

    Create opd-voucher.model.ts

---
+++ woms.client/src/app/core/models/master/opd-voucher.model.ts
@@ -0,0 +1,74 @@
+export interface OPDVoucherModel {
+    opdvno: string;
+    branchId: number;
+    patientId?: string | null;
+    vdate?: string | null; // ISO Date string
+    doctorId?: number | null;
+    totalAmount: number;
+    discountAmount: number;
+    paidAmount: number;
+    leftAmount: number;
+    paymentType: string;
+    createdOn?: string | null;
+    createdBy?: string | null;
+    updatedOn?: string | null;
+    updatedBy?: string | null;
+    deletedOn?: string | null;
+    deletedBy?: string | null;
+    remark?: string | null;
+    status?: string | null;
+}
+
+export interface OPDVoucherItemModel {
+    opdvno: string;
+    serviceId: number;
+    consultationId: string;
+    result?: string | null;
+    resultDate?: string | null;
+    quantity?: number | null;
+    unitPrice?: number | null;
+    amount?: number | null;
+}
+
+// The combined payload you will send to your Create/Update API
+export interface OPDVoucherEntryModel extends OPDVoucherModel {
+    items: OPDVoucherItemModel[];
+}
+
+export interface ViOPDVoucherModel {
+    opdvno: string;
+    branchId: number;
+    patientId?: string | null;
+    vdate?: string | null;
+    doctorId?: number | null;
+    totalAmount: number;
+    discountAmount: number;
+    paidAmount: number;
+    leftAmount: number;
+    paymentType: string;
+    createdOn?: string | null;
+    createdBy?: string | null;
+    updatedOn?: string | null;
+    updatedBy?: string | null;
+    deletedOn?: string | null;
+    deletedBy?: string | null;
+    remark?: string | null;
+    status?: string | null;
+    // Joined Display Fields
+    patientName: string;
+    doctorName?: string | null;
+}
+
+export interface ViOPDVoucherItemModel {
+    consultantFee?: number | null;
+    serviceName?: string | null;
+    serviceFee?: number | null;
+    opdvno: string;
+    serviceId: number;
+    consultationId: string;
+    result?: string | null;
+    resultDate?: string | null;
+    quantity?: number | null;
+    unitPrice?: number | null;
+    totalItemAmount?: number | null;
+}
