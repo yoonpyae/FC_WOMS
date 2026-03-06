d1f96bd15b5e0e5ca932beec77f20640db51b6ea
ypeain <yoonpyae26@gmail.com>
2026-03-07 01:13:48 +0630

    Update voucher-item.model.ts

--- woms.client/src/app/core/models/voucher-item.model.ts
+++ woms.client/src/app/core/models/voucher-item.model.ts
@@ -4,16 +4,5 @@ export interface VoucherItemModel {
     price: number;
     qty: number;
     total: number;
-    type: "Consultant" | "Round" | "OPD Service" | "Ward Service" | "ECG" | "X-Ray" | "Ultrasound" | "Physiotherapy" | "Pharmacy";
-}
-
-export interface OTVoucherItemModel {
-    id: string;
-    name: string;
-    typecode: number;
-    typename: string,
-    price: number;
-    qty: number;
-    total: number;
-    type: "OT Stock" | "OT Service";
+    type: "Consultation" | "Service";
 }
