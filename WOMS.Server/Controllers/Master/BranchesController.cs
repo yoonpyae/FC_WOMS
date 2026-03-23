dac0642cb59f94b9c45958acf264c9bc1d2b9eb1
ypeain <yoonpyae26@gmail.com>
2026-03-23 21:01:24 +0630

    Update BranchesController.cs

--- WOMS.Server/Controllers/Master/BranchesController.cs
+++ WOMS.Server/Controllers/Master/BranchesController.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WOMS.Server.Controllers.Master;
+﻿namespace WOMS.Server.Controllers.Master;
 
 [Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class BranchesController(IRepositoryWrapper repo) : ControllerBase
 {
-
     #region CRUD Operation
 
     [HttpGet]
@@ -34,17 +30,39 @@ public class BranchesController(IRepositoryWrapper repo) : ControllerBase
     [HttpPost]
     [ValidateModel]
     [EndpointSummary("Create")]
-    [EndpointDescription("Creates new Branch Test.")]
+    [EndpointDescription("Creates new Branch.")]
     public async Task<IActionResult> Create(Branch model)
     {
+        Branch? existingBranch = await repo.Branches.GetFirstAsync(x =>
+            x.BranchName.ToLower() == model.BranchName.ToLower() && !x.DeletedOn.HasValue);
+
+        if (existingBranch != null)
+        {
+            return ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("A branch with this name already exists.", ""));
+        }
+
+        if (model.IsDefault)
+        {
+            IReadOnlyList<Branch>? currentDefaults = await repo.Branches.GetAsync(x => x.IsDefault && !x.DeletedOn.HasValue);
+            foreach (Branch def in currentDefaults)
+            {
+                def.IsDefault = false;
+                repo.Branches.Update(def);
+            }
+        }
+
+        Branch? lastRecord = await repo.Branches.GetFirstAsync(null, q => q.OrderByDescending(x => x.BranchId));
+        model.BranchId = (lastRecord?.BranchId ?? 0) + 1;
+
         model.CreatedOn = DateTime.Now;
         model.CreatedBy = User.Identity?.Name ?? string.Empty;
 
         repo.Branches.Create(model);
+
         return await repo.SaveAsync()
             ? ResponseHelper.Created_Result("/api/Branch", null,
                 new DefaultResponseMessageModel("Successfully created new Branch.", ""))
-            : ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("Unable to create Branch", ""));
+            : ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("Unable to create Branch.", ""));
     }
 
     [HttpPut]
@@ -53,9 +71,31 @@ public class BranchesController(IRepositoryWrapper repo) : ControllerBase
     [EndpointDescription("Updates an existing Branch.")]
     public async Task<IActionResult> Update(Branch model)
     {
-        var branch = await repo.Branches.GetFirstAsync(x => x.BranchId == model.BranchId);
+        Branch? branch = await repo.Branches.GetFirstAsync(x => x.BranchId == model.BranchId);
         if (branch == null)
+        {
             return ResponseHelper.NotFound_Request(null, new DefaultResponseMessageModel("Branch not found.", ""));
+        }
+
+        Branch? duplicateName = await repo.Branches.GetFirstAsync(x =>
+            x.BranchName.ToLower() == model.BranchName.ToLower() &&
+            x.BranchId != model.BranchId &&
+            !x.DeletedOn.HasValue);
+
+        if (duplicateName != null)
+        {
+            return ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("Another branch is already using this name.", ""));
+        }
+
+        if (model.IsDefault && !branch.IsDefault)
+        {
+            IReadOnlyList<Branch>? currentDefaults = await repo.Branches.GetAsync(x => x.IsDefault && x.BranchId != model.BranchId && !x.DeletedOn.HasValue);
+            foreach (var def in currentDefaults)
+            {
+                def.IsDefault = false;
+                repo.Branches.Update(def);
+            }
+        }
 
         // Update fields
         branch.BranchName = model.BranchName;
@@ -78,7 +118,7 @@ public class BranchesController(IRepositoryWrapper repo) : ControllerBase
 
         return await repo.SaveAsync()
             ? ResponseHelper.OK_Result(null, new DefaultResponseMessageModel("Successfully updated Branch.", ""))
-            : ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("Unable to update Branch", ""));
+            : ResponseHelper.Bad_Request(null, new DefaultResponseMessageModel("Unable to update Branch.", ""));
     }
 
     #endregion
