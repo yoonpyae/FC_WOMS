55d5524ad5432cbb8811d7fb4ef272323fdc0517
ypeain <yoonpyae26@gmail.com>
2026-04-09 22:42:02 +0630

    Update Program.cs

--- WOMS.Server/Program.cs
+++ WOMS.Server/Program.cs
@@ -63,7 +63,7 @@ app.MapScalarApiReference("/docs", options =>
 
     if (app.Environment.IsDevelopment())
     {
-        _ = options.AddServer(new ScalarServer("https://localhost:7126")
+        _ = options.AddServer(new ScalarServer("http://13.212.206.216:5003")
         { Description = "UAT Local Mode" }); // THis is  Testing Mode For Analystic
     }
 });
