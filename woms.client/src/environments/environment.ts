01cb3c4dddcfffff13995bf2d0a7907ec1446838
ypeain <yoonpyae26@gmail.com>
2026-04-13 23:45:58 +0630

    Update environment.ts

--- woms.client/src/environments/environment.ts
+++ woms.client/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   docker: false,
 
-  main_url: 'http://13.212.206.216:5003/api',
+  main_url: 'http://13.212.76.8:5003/api',
   web_url: 'https://localhost:7126',
 
   debug: true,
